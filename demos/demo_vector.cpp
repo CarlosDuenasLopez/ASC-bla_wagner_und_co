62af1a44534a050ce54cae983b746a02849824f4
CarlosDuenasLopez <caaldulo@gmail.com>
2025-10-08 17:11:54 +0200

    MINUS

--- demos/demo_vector.cpp
+++ demos/demo_vector.cpp
@@ -4,19 +4,19 @@
 
 namespace bla = ASC_bla;
 
-
 int main()
 {
   size_t n = 5;
   bla::Vector<double> x(n), y(n);
 
   for (size_t i = 0; i < x.Size(); i++)
-    {
-      x(i) = i;
-      y(i) = 10;
-    }
+  {
+    x(i) = i;
+    y(i) = 10;
+  }
 
-  bla::Vector<double> z = x+y;
-  
-  std::cout << "x+y = " << z << std::endl;
+  bla::Vector<double> z = x + y;
+  bla::Vector<double> a = x - y;
+
+  std::cout << "x-y = " << a << std::endl;
 }
--- src/vector.hpp
+++ src/vector.hpp
@@ -5,63 +5,71 @@
 
 namespace ASC_bla
 {
-  
+
   template <typename T>
   class Vector
   {
     size_t size;
-    T * data;
-    
+    T *data;
+
   public:
-    Vector (size_t _size) 
-      : size(_size), data(new T[size]) { ; }
-    
-    Vector (const Vector & v)
-      : Vector(v.Size())
+    Vector(size_t _size)
+        : size(_size), data(new T[size]) { ; }
+
+    Vector(const Vector &v)
+        : Vector(v.Size())
     {
       *this = v;
     }
 
-    Vector (Vector && v)
-      : size(0), data(nullptr)
+    Vector(Vector &&v)
+        : size(0), data(nullptr)
     {
       std::swap(size, v.size);
       std::swap(data, v.data);
     }
 
-    ~Vector () { delete [] data; }
-    
-    Vector & operator=(const Vector & v2)
+    ~Vector() { delete[] data; }
+
+    Vector &operator=(const Vector &v2)
     {
       for (size_t i = 0; i < size; i++)
         data[i] = v2(i);
       return *this;
     }
 
-    Vector & operator= (Vector && v2)
+    Vector &operator=(Vector &&v2)
     {
       std::swap(size, v2.size);
       std::swap(data, v2.data);
       return *this;
     }
-    
+
     size_t Size() const { return size; }
-    T & operator()(size_t i) { return data[i]; }
-    const T & operator()(size_t i) const { return data[i]; }
+    T &operator()(size_t i) { return data[i]; }
+    const T &operator()(size_t i) const { return data[i]; }
   };
 
-
   template <typename T>
-  Vector<T> operator+ (const Vector<T> & a, const Vector<T> & b)
+  Vector<T> operator+(const Vector<T> &a, const Vector<T> &b)
   {
     Vector<T> sum(a.Size());
     for (size_t i = 0; i < a.Size(); i++)
-      sum(i) = a(i)+b(i);
+      sum(i) = a(i) + b(i);
     return sum;
   }
-  
+
   template <typename T>
-  std::ostream & operator<< (std::ostream & ost, const Vector<T> & v)
+  Vector<T> operator-(const Vector<T> &a, const Vector<T> &b)
+  {
+    Vector<T> sum(a.Size());
+    for (size_t i = 0; i < a.Size(); i++)
+      sum(i) = a(i) - b(i);
+    return sum;
+  }
+
+  template <typename T>
+  std::ostream &operator<<(std::ostream &ost, const Vector<T> &v)
   {
     if (v.Size() > 0)
       ost << v(0);
@@ -69,7 +77,7 @@ namespace ASC_bla
       ost << ", " << v(i);
     return ost;
   }
-  
+
 }
 
 #endif
